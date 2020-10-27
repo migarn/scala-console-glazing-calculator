29781d39abbc7ff0bfa16d0057c0417cd3a2f980
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-27 18:54:11 +0100

    Method scanPositiveInt commented

--- src/com/michalgarnczarski/InputScanner.scala
+++ src/com/michalgarnczarski/InputScanner.scala
@@ -6,6 +6,9 @@ import scala.util.Try
 object InputScanner {
 
   def scanPositiveInt(): Int = {
+
+    // If input is not Int or is negative return -1
+
     val input: Int = Try(readInt()).orElse(Try(-1)).get
     input.max(-1)
   }
