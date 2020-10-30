06f000c4cac63614c543bf7b16c275e44c29c9ac
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-30 17:51:01 +0100

    ConsoleApplication commented. createReport() extracted from run()

--- src/com/michalgarnczarski/ConsoleApplication.scala
+++ src/com/michalgarnczarski/ConsoleApplication.scala
@@ -4,9 +4,9 @@ import com.michalgarnczarski.InputScanner._
 
 object ConsoleApplication {
 
-  // to comment and test
-
   def run(): Unit = {
+
+    // Function launching console user interface
 
     println("\nThe application defines minimum thickness of a glazing for given dimensions.")
 
@@ -25,51 +25,53 @@ object ConsoleApplication {
       val loopController: Int = scanIntForSelectionList("\nType:\n1 - to continue\n2 - to exit",1,2)
       if (loopController == 2) inLoop = false
     }
+  }
 
-    def createReport(glass: Glass): String = {
+  private def createReport(glass: Glass): String = {
 
-      val thickness = new GlassThicknessDefiner(glass).defineThickness
-      val surcharge = new GlassSurchargeDefiner(glass).defineSurcharge
+    // Auxiliary function creating report for given glazing parameters
 
-      val report: StringBuilder = new StringBuilder
+    val thickness = new GlassThicknessDefiner(glass).defineThickness
+    val surcharge = new GlassSurchargeDefiner(glass).defineSurcharge
 
-      report.append("\n-------------------------------------------")
-      report.append("\n\nAssumed parameters:")
-      report.append("\n\t- dimensions: " + glass.width + " mm x " + glass.height + " mm,")
-      report.append("\n\t- area: " + glass.area + " m2,")
-      report.append("\n\t- spacer thickness: " + glass.spacerThickness + " mm.")
+    val report: StringBuilder = new StringBuilder
 
-      if (thickness == -1 || surcharge == -1) {
-        report.append("\n\nPlease consult assumed parameters with the glazing manufacturer!")
-      }
+    report.append("\n-------------------------------------------")
+    report.append("\n\nAssumed parameters:")
+    report.append("\n\t- dimensions: " + glass.width + " mm x " + glass.height + " mm,")
+    report.append("\n\t- area: " + glass.area + " m2,")
+    report.append("\n\t- spacer thickness: " + glass.spacerThickness + " mm.")
 
-      else {
-        val vsgThickness: String = thickness match {
-          case 3 => "33.1"
-          case 4 => "33.1"
-          case 5 => "44.1"
-          case 6 => "55.1"
-          case 8 => "66.1"
-          case 10 => "88.1"
-          case 12 => "10.10.1"
-        }
-
-        report.append("\n\nFor assumed parameters minimum glazing width is:")
-        report.append("\n\t- " + thickness + " mm - for single glass,")
-        report.append("\n\t- " + vsgThickness + " - for laminated glass.")
-
-        if (surcharge == 0)
-          report.append("\n\nFor assumed parameters there is no surcharge for oversizing.")
-
-        else
-          report.append("\n\nFor assumed parameters there is " + surcharge + "% surcharge for oversizing!")
-
-        if (glass.dimensionsRatio < 0.1)
-          report.append("\n\nAttention! Glazing dimensions ratio is lower than 1:10!")
-      }
-
-      report.append("\n\n-------------------------------------------")
-      report.toString()
+    if (thickness == -1 || surcharge == -1) {
+      report.append("\n\nPlease consult assumed parameters with the glazing manufacturer!")
     }
+
+    else {
+      val vsgThickness: String = thickness match {
+        case 3 => "33.1"
+        case 4 => "33.1"
+        case 5 => "44.1"
+        case 6 => "55.1"
+        case 8 => "66.1"
+        case 10 => "88.1"
+        case 12 => "10.10.1"
+      }
+
+      report.append("\n\nFor assumed parameters minimum glazing width is:")
+      report.append("\n\t- " + thickness + " mm - for single glass,")
+      report.append("\n\t- " + vsgThickness + " - for laminated glass.")
+
+      if (surcharge == 0)
+        report.append("\n\nFor assumed parameters there is no surcharge for oversizing.")
+
+      else
+        report.append("\n\nFor assumed parameters there is " + surcharge + "% surcharge for oversizing!")
+
+      if (glass.dimensionsRatio < 0.1)
+        report.append("\n\nAttention! Glazing dimensions ratio is lower than 1:10!")
+    }
+
+    report.append("\n\n-------------------------------------------")
+    report.toString()
   }
 }
