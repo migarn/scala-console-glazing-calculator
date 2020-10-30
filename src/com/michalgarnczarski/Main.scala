97a9f14339dcf3cffc7c6f9b68187dd3d51d1835
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-10-30 17:25:09 +0100

    Main cleaned

--- src/com/michalgarnczarski/Main.scala
+++ src/com/michalgarnczarski/Main.scala
@@ -5,19 +5,5 @@ object Main {
 
     ConsoleApplication.run
 
-
-
-//    println("\nThe application defines minimum thickness of glazing for given dimensions.")
-//
-//    val glass = new Glass(2001, 1100, 8)
-//    println(glass.area)
-//    println(glass.dimensionsRatio)
-//
-//    val glassThicknessDefiner = new GlassThicknessDefiner(glass)
-//    println(glassThicknessDefiner.thickness)
-//
-//    println("Podaj liczbę:")
-//    println(InputScanner.scanPositiveInt())
-
   }
 }
